e00a86a6e7a92e052a45c0d9df97b9d94a8b9ab1
battal <battal51280@gmail.com>
2021-07-01 18:18:01 +0300

    api: sections and solutions done

---
+++ api/sections/[parent_id].go
@@ -0,0 +1,46 @@
+package handler
+
+import (
+	"encoding/json"
+	"log"
+	"strconv"
+	"net/http"
+)
+
+type Section struct {
+	Id        string  `json:"_id"`
+	Title     string  `json:"title"`
+	Parent_id string  `json:"parent_id"`
+  Is_parent bool    `json:"is_parent"` // false: it is not a parent to another section, it contains just solutions 
+}
+
+func Handler(w http.ResponseWriter, r *http.Request) {
+  log.Println("[REQUEST]")
+
+	// Make Request
+	resp, err := http.Get("http://hizrenkvideo.frns.in/controller/get-source.php?action=source_list&id=" + r.URL.Query().Get("parent_id"))
+	if err != nil {
+		http.Error(w, err.Error(), resp.StatusCode)
+		return
+  }
+	defer resp.Body.Close()
+
+	// Parse Request
+	var body map[string]interface{}
+	if err := json.NewDecoder(resp.Body).Decode(&body); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+  }
+	var sections = make([]Section, len(body["sources"].([]interface{})))
+	for i := 0; i < len(sections); i++ {
+		// id -> _id ; pid -> parent_id ; parent -> is_parent etc.
+		sect := body["sources"].([]interface{})[i].(map[string]interface{})
+		sections[i].Id = sect["id"].(string)
+		sections[i].Parent_id = sect["pid"].(string)
+		sections[i].Is_parent, _ = strconv.ParseBool((sect["parent"].(string)))
+		sections[i].Title = sect["nm"].(string)
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+  json.NewEncoder(w).Encode(sections)
+}
---
+++ api/solutions/[parent_id].go
@@ -0,0 +1,51 @@
+package handler
+
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+)
+
+type Solution struct {
+	Id            string  `json:"_id"`
+	Title         string  `json:"title"`
+	Solution_id   string  `json:"solution_id"`
+	Solution_type string  `json:"solution_type"`
+	Audio         string  `json:"audio"`
+	SWF           string  `json:"swf"`
+	XAML          string  `json:"xaml"`
+}
+
+func Handler(w http.ResponseWriter, r *http.Request) {
+  log.Println("[REQUEST]")
+
+	// Make Request
+	resp, err := http.Get("http://hizrenkvideo.frns.in/controller/get-source.php?action=content_list&id=" + r.URL.Query().Get("parent_id"))
+	if err != nil {
+		http.Error(w, err.Error(), resp.StatusCode)
+		return
+  }
+	defer resp.Body.Close()
+
+	// Parse Request
+	var body map[string]interface{}
+	if err := json.NewDecoder(resp.Body).Decode(&body); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+  }
+	var solutions = make([]Solution, len(body["contents"].([]interface{})))
+	for i := 0; i < len(solutions); i++ {
+		// id -> _id ; pid -> parent_id ; parent -> is_parent etc.
+		solution := body["contents"].([]interface{})[i].(map[string]interface{})
+		solutions[i].Id = solution["id"].(string)
+		solutions[i].Title = solution["nm"].(string)
+		solutions[i].Solution_id = solution["solved_id"].(string)
+		solutions[i].Solution_type = solution["solved_type"].(string)
+		solutions[i].Audio = solution["audio"].(string)
+		solutions[i].SWF = solution["swf"].(string)
+		solutions[i].XAML = solution["video"].(string)
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+  json.NewEncoder(w).Encode(solutions)
+}
