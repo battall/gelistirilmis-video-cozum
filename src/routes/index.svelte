77602493cc55668303250da44f7737348d332406
battal <battal51280@gmail.com>
2021-07-05 18:24:51 +0300

    site: responsive player: prev-next buttons

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,12 +1,16 @@
 <script>
+  let test = [
+    { _id: "35873", title: "amatör-3", parent_id: "35106", is_parent: false },
+  ];
+
   import { onMount } from "svelte";
   import { session } from "$app/stores";
   import Player from "$lib/Player/index.svelte";
 
   onMount(() => {
-    console.log("INDEX MOUNT", $session);
     if ($session.navPath.length === 0)
       updateNav([{ _id: 2, title: "Kitaplar", is_parent: true }]);
+    // updateNav(test);
   });
 
   let updateNav = (navPath) => {
@@ -28,7 +32,7 @@
 </svelte:head>
 
 <div class="index">
-  <nav>
+  <nav class:active={!$session.solution}>
     <ul class="book-sections">
       {#each $session.navItems as item}
         <li
@@ -36,6 +40,7 @@
           on:click={() =>
             item.solution_id
               ? ($session.solution = {
+                  ...item,
                   pdf: `/api/proxy?publisher=${
                     $session.publisher
                   }&path=${item.swf.replace(".swf", ".pdf")}`,
@@ -50,8 +55,48 @@
     </ul>
   </nav>
 
-  <main>
+  <main class:active={$session.solution}>
     <Player solution={$session.solution} />
+    <div class="prev-next">
+      <button
+        on:click={() => {
+          let currentIndex = $session.navItems.findIndex(
+            (item) => item.solution_id === $session.solution.solution_id
+          );
+          let item = $session.navItems[currentIndex - 1];
+          if (!item) return;
+          $session.solution = {
+            ...item,
+            pdf: `/api/proxy?publisher=${
+              $session.publisher
+            }&path=${item.swf.replace(".swf", ".pdf")}`,
+            xml: `/api/proxy?publisher=${$session.publisher}&path=${item.xml}`,
+            audio: `/api/proxy?publisher=${$session.publisher}&path=${item.audio}`,
+          };
+        }}
+      >
+        Önceki
+      </button>
+      <button
+        on:click={() => {
+          let currentIndex = $session.navItems.findIndex(
+            (item) => item.solution_id === $session.solution.solution_id
+          );
+          let item = $session.navItems[currentIndex + 1];
+          if (!item) return;
+          $session.solution = {
+            ...item,
+            pdf: `/api/proxy?publisher=${
+              $session.publisher
+            }&path=${item.swf.replace(".swf", ".pdf")}`,
+            xml: `/api/proxy?publisher=${$session.publisher}&path=${item.xml}`,
+            audio: `/api/proxy?publisher=${$session.publisher}&path=${item.audio}`,
+          };
+        }}
+      >
+        Sonraki
+      </button>
+    </div>
   </main>
 </div>
 
@@ -61,13 +106,16 @@
   }
 
   nav {
-    display: flex;
+    display: none;
     flex-direction: column;
 
-    width: 24%;
+    width: 100%;
     min-height: calc(100vh - 40pt);
 
     border-right: 1px solid var(--accents-2);
+  }
+  nav.active {
+    display: flex;
   }
 
   nav .book-sections {
@@ -97,11 +145,43 @@
   }
 
   main {
-    display: flex;
+    display: none;
+    justify-content: space-between;
+    flex-direction: column;
 
-    width: 76%;
-    min-height: calc(100vh - 40pt);
+    width: 100%;
+    min-height: calc(100vh - var(--header-height));
 
     background-color: #f9f9f9;
+  }
+  main.active {
+    display: flex;
+  }
+  main .prev-next {
+    display: flex;
+    flex-shrink: 0;
+
+    border-top: 1px solid var(--accents-3);
+  }
+  main .prev-next button {
+    width: 50%;
+    height: var(--header-height);
+
+    margin: 0;
+    padding: 0;
+    border: 0;
+    background-color: white;
+
+    border: 1px solid var(--accents-2);
+    border-width: 0 1px 0 1px;
+  }
+
+  @media only screen and (min-width: 480px) {
+    nav {
+      width: 24%;
+    }
+    main {
+      width: 76%;
+    }
   }
 </style>
